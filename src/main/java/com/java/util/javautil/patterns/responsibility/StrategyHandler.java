6655352913d71a8e7cd40e73c627ca26f80da06e
BryceHu <1370054591@qq.com>
2021-01-28 21:19:50 +0800

    if-else

---
+++ src/main/java/com/java/util/javautil/patterns/responsibility/StrategyHandler.java
@@ -0,0 +1,20 @@
+package com.java.util.javautil.patterns.responsibility;
+
+
+/**
+ * @author
+ * @date
+ */
+public interface StrategyHandler<T, R> {
+
+    @SuppressWarnings("rawtypes")
+    StrategyHandler DEFAULT = t -> null;
+
+    /**
+     * apply strategy
+     *
+     * @param param
+     * @return
+     */
+    R apply(T param);
+}
