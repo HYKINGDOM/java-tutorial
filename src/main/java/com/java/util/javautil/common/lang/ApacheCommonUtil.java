bd10dbc33f18d50c1f526ca815e6abe4908d479d
1370054591@qq.com <Hy852992207>
2021-02-12 18:35:42 +0800

    add hutool demo

--- src/main/java/com/java/util/javautil/common/lang/ApacheCommonUtil.java
+++ src/main/java/com/java/util/javautil/common/lang/ApacheCommonUtil.java
@@ -5,8 +5,11 @@ import org.apache.commons.lang3.StringUtils;
 public class ApacheCommonUtil {
 
     public static void main(String[] args) {
-        StringUtils.replace("tdadasd","a","b");
-        String str = "dasdada";
-        str.replace("dasda","a");
+        String str02 = new StringBuilder("测试").append("wenjian").toString();
+        System.out.println(str02.intern() == str02);
+
+
+        String str01 = new StringBuilder("ja").append("va").toString();
+        System.out.println(str01.intern() == str01);
     }
 }
