daeda32d466378cf499e9726f1cd107e16cf7587
BryceH <1370054591@qq.com>
2021-12-08 20:52:49 +0800

    commit modify

--- src/main/java/com/java/util/javautil/DDD/alibaba/User.java
+++ src/main/java/com/java/util/javautil/DDD/alibaba/User.java
@@ -17,7 +17,6 @@ public class User {
     public User register(@NotNull UserId userId, @NotNull PhoneNumber phone) {
 
         UserService userService = new UserServiceImpl();
-
         // 最后创建用户，落盘，然后返回，这部分代码实际上也能用Builder解决
         User user = new User();
         user.userId = userId;
--- src/main/java/com/java/util/javautil/date/DateFormatDemo.java
+++ src/main/java/com/java/util/javautil/date/DateFormatDemo.java
@@ -1,7 +1,14 @@
 package com.java.util.javautil.date;
 
+import org.apache.commons.lang3.reflect.FieldUtils;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
 /**
  * @author HY
@@ -9,15 +16,22 @@ import java.time.format.DateTimeFormatter;
 public class DateFormatDemo {
 
 
-    public static void main(String[] args) {
-        DateFormatDemo dateFormatDemo = new DateFormatDemo();
-        dateFormatDemo.dateFormatLocal();
+    public static void main(String[] args) throws ParseException {
+
+
     }
 
-    public void dateFormatLocal(){
+
+    private void sqlDateConvertToUtilDate() {
+        String str = "2021-04-05";
+        Date newDate = java.sql.Date.valueOf(str);
+    }
+
+
+    public void dateFormatLocal() {
         DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
         String dateStr = "2019-01-01 00:00:00";
-        LocalDateTime localDateTime = LocalDateTime.parse(dateStr,dateTimeFormatter);
+        LocalDateTime localDateTime = LocalDateTime.parse(dateStr, dateTimeFormatter);
         System.out.println(localDateTime);
     }
 }
