8095405224650520ea09c29030298351e048f601
BryceH <1370054591@qq.com>
2022-04-30 10:39:15 +0800

    commit modify

--- src/main/java/com/java/util/javautil/validator/Phone.java
+++ src/main/java/com/java/util/javautil/validator/Phone.java
@@ -7,6 +7,9 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * @author HY
+ */
 @Target({ElementType.FIELD})
 @Retention(RetentionPolicy.RUNTIME)
 @Constraint(validatedBy = PhoneValidator.class)
