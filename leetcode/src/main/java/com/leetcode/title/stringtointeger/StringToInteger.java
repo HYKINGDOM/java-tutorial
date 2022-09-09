79909a25fe7fdeb711d5cdee73a038cdeefdb1dd
yihurCoder <1370054591@qq.com>
2022-09-09 13:50:22 +0800

    modify package

---
+++ leetcode/src/main/java/com/leetcode/title/stringtointeger/StringToInteger.java
@@ -0,0 +1,98 @@
+package com.leetcode.title.stringtointeger;
+
+public class StringToInteger {
+
+    public int myAtoi(String s) {
+
+        char[] chars = s.toCharArray();
+
+        int first = -1;
+        int end = -1;
+        int num = 0;
+
+        boolean flag = false;
+        for (int i = 0; i < chars.length; i++) {
+            String valueOf = String.valueOf(chars[i]);
+
+            if (!Character.isDigit(chars[i])) {
+
+                if ((" ").equals(valueOf)) {
+                    if (first == -1) {
+                        continue;
+                    }
+                    break;
+                }
+                num++;
+                if (num >= 2) {
+                    if (first == -1) {
+                        return 0;
+                    }
+                    break;
+                }
+                if ("-".equals(valueOf)) {
+                    if (first == -1) {
+                        flag = true;
+                        continue;
+                    }
+                    break;
+                }
+                if ("+".equals(valueOf)) {
+                    if (first == -1) {
+                        continue;
+                    }
+                    break;
+                }
+                if (".".equals(valueOf)) {
+                    if (first == -1) {
+                        break;
+                    }
+                    end = i - 1;
+                    break;
+                }
+            }
+
+
+            if (Character.isDigit(chars[i])) {
+                if (first == -1) {
+                    if ((int) chars[i] == 0){
+                        continue;
+                    }
+                    first = i;
+                    end = i;
+                    continue;
+                }
+
+                end = i;
+            }
+        }
+
+        if (first == -1) {
+            return 0;
+        }
+
+        String substring = s.substring(first, end + 1);
+
+        if (flag) {
+            substring = "-" + substring;
+        }
+
+        if (substring.length() > 13){
+            if (flag){
+                return Integer.MIN_VALUE;
+            }
+            return Integer.MAX_VALUE;
+        }
+
+        long parseLong = Long.parseLong(substring);
+
+        if (parseLong < Integer.MIN_VALUE) {
+            return Integer.MIN_VALUE;
+        }
+
+        if (parseLong > Integer.MAX_VALUE) {
+            return Integer.MAX_VALUE;
+        }
+
+        return Integer.parseInt(substring);
+    }
+}
---
+++ leetcode/src/test/java/com/leetcode/title/stringtointeger/StringToIntegerTest.java
@@ -0,0 +1,135 @@
+package com.leetcode.title.stringtointeger;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class StringToIntegerTest {
+
+    private StringToInteger stringToInteger;
+
+
+    @BeforeEach
+    public void init() {
+        stringToInteger = new StringToInteger();
+    }
+
+
+    @Test
+    public void test_main_01() {
+        String str = "42";
+        int profit = stringToInteger.myAtoi(str);
+        assertThat(profit).isEqualTo(42);
+    }
+
+
+    @Test
+    public void test_main_02() {
+        String str = "   -42";
+        int profit = stringToInteger.myAtoi(str);
+        assertThat(profit).isEqualTo(-42);
+    }
+
+    @Test
+    public void test_main_03() {
+        String str = "4193 with words";
+        int profit = stringToInteger.myAtoi(str);
+        assertThat(profit).isEqualTo(4193);
+    }
+
+    @Test
+    public void test_main_04() {
+        String str = "words and 987";
+        int profit = stringToInteger.myAtoi(str);
+        assertThat(profit).isEqualTo(0);
+    }
+
+
+    @Test
+    public void test_main_05() {
+        String str = "-91283472332";
+        int profit = stringToInteger.myAtoi(str);
+        assertThat(profit).isEqualTo(Integer.MIN_VALUE);
+    }
+
+
+    @Test
+    public void test_main_06() {
+
+        String str = "3.14159";
+        int profit = stringToInteger.myAtoi(str);
+        assertThat(profit).isEqualTo(3);
+    }
+
+
+    @Test
+    public void test_main_07() {
+
+        String str = "";
+        int profit = stringToInteger.myAtoi(str);
+        assertThat(profit).isEqualTo(0);
+    }
+
+
+    @Test
+    public void test_main_08() {
+
+        String str = "1";
+        int profit = stringToInteger.myAtoi(str);
+        assertThat(profit).isEqualTo(1);
+    }
+
+    @Test
+    public void test_main_09() {
+
+        String str = "+1";
+        int profit = stringToInteger.myAtoi(str);
+        assertThat(profit).isEqualTo(1);
+    }
+
+    @Test
+    public void test_main_10() {
+
+        String str = "+-1";
+        int profit = stringToInteger.myAtoi(str);
+        assertThat(profit).isEqualTo(0);
+    }
+
+    @Test
+    public void test_main_11() {
+
+        String str = "00000-42a1234";
+        int profit = stringToInteger.myAtoi(str);
+        assertThat(profit).isEqualTo(0);
+    }
+
+    @Test
+    public void test_main_12() {
+        String str = ".1";
+        int profit = stringToInteger.myAtoi(str);
+        assertThat(profit).isEqualTo(0);
+    }
+
+    @Test
+    public void test_main_13() {
+        String str = "   +0 123";
+        int profit = stringToInteger.myAtoi(str);
+        assertThat(profit).isEqualTo(0);
+    }
+
+    @Test
+    public void test_main_14() {
+        String str = "20000000000000000000";
+        int profit = stringToInteger.myAtoi(str);
+        assertThat(profit).isEqualTo(Integer.MAX_VALUE);
+    }
+
+
+    @Test
+    public void test_main_15() {
+        String str = "  0000000000012345678";
+        int profit = stringToInteger.myAtoi(str);
+        assertThat(profit).isEqualTo(12345678);
+    }
+}
