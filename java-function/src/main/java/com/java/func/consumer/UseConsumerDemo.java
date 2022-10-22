5f482da1876fc40205858e5aa701208e214d4c17
yihurCoder <1370054591@qq.com>
2022-10-22 15:56:36 +0800

    modify package

---
+++ java-function/src/main/java/com/java/func/consumer/UseConsumerDemo.java
@@ -0,0 +1,25 @@
+package com.java.func.consumer;
+
+import java.util.function.Consumer;
+import java.util.stream.Stream;
+
+public class UseConsumerDemo {
+
+
+    public static void main(String[] args) {
+
+        Consumer<String> consumer = System.out::println;
+        Stream<String> stream = Stream.of("aaa", "bbb", "ddd", "ccc", "fff");
+        stream.forEach(consumer);
+
+        stream.forEach(UseConsumerDemo::listToString);
+
+
+    }
+
+
+    private static String listToString(String str){
+
+        return str.toUpperCase();
+    }
+}
