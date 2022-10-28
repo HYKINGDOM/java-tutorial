5f81a03aacf9bb4f663494e6fffb8cf1b4433180
yihurCoder <1370054591@qq.com>
2022-10-28 11:42:31 +0800

    modify code

---
+++ java-common/src/main/java/com/java/coco/utils/html/MHTMLUtil.java
@@ -0,0 +1,20 @@
+package com.java.coco.utils.html;
+
+
+import org.apache.james.mime4j.dom.Message;
+
+import org.apache.james.mime4j.dom.Multipart;
+
+import org.apache.james.mime4j.dom.field.ContentTypeField;
+
+import org.apache.james.mime4j.message.DefaultMessageBuilder;
+
+import org.apache.james.mime4j.stream.MimeConfig;
+
+import java.io.File;
+
+
+public class MHTMLUtil {
+
+
+}
