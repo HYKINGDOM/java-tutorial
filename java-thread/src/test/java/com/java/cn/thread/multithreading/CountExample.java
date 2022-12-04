fbf2394e4d876ffe5566b1560fe52f455903ce15
yihurCoder <1370054591@qq.com>
2022-12-04 11:01:21 +0800

    import UT lib project file

--- java-thread/src/test/java/com/java/cn/thread/multithreading/CountExample.java
+++ java-thread/src/test/java/com/java/cn/thread/multithreading/CountExample.java
@@ -1,7 +1,7 @@
 package com.java.cn.thread.multithreading;
 
 import lombok.extern.slf4j.Slf4j;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
