979afa8f7364760f414cb8b6c9affef943348200
yihurCoder <1370054591@qq.com>
2022-12-06 23:38:59 +0800

    function demo file

--- java-function/src/test/java/com/java/func/FunctionTest.java
+++ java-function/src/test/java/com/java/func/FunctionTest.java
@@ -3,9 +3,12 @@ package com.java.func;
 import cn.hutool.core.util.RandomUtil;
 import cn.hutool.core.util.StrUtil;
 import com.java.coco.utils.RanDomCollectUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.Test;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.Locale;
 import java.util.function.BiConsumer;
 import java.util.function.BiFunction;
 import java.util.function.Consumer;
@@ -21,19 +24,46 @@ public class FunctionTest {
 
     @Test
     public void test_Function() {
-        Supplier<List<String>> dataSupplier = () -> RanDomCollectUtils.randomCollectList(13,100);
+        Supplier<List<String>> dataSupplier = () -> RanDomCollectUtils.randomCollectList(13, 100);
 
 
         Predicate<String> dataPredicate = e -> e.contains("Z");
 
 
-        Consumer<String> dataConsumer = s -> s = s.toLowerCase();
+        Consumer<String> dataConsumer = e -> {
+            e = e.toUpperCase(Locale.ROOT);
+            System.out.println(e);
+        };
+
+
+        Function<String,String> stringFunction = new Function<String, String>() {
+            @Override
+            public String apply(String s) {
+                return s.toUpperCase();
+            }
+        };
+
+
+
+        Comparator<String> stringComparator = new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+
+                return StringUtils.compareIgnoreCase(o1.substring(0, 1), o2.substring(0, 1));
+            }
+        };
 
 
         List<String> stringList = dataSupplier.get();
 
         assert stringList != null;
-        List<String> stringList1 = stringList.stream().filter(dataPredicate).peek(dataConsumer).collect(Collectors.toList());
+        List<String> stringList1 = stringList.stream()
+                .filter(dataPredicate)
+//                .peek(dataConsumer)
+                .map(stringFunction)
+                .sorted(stringComparator)
+//                .peek(dataConsumer)
+                .collect(Collectors.toList());
 
 
         System.out.println(stringList1.size());
