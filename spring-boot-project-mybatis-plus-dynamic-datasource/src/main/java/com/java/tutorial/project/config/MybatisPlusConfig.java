feac043b388b742dd7c63a2394e781f1b8483ab4
yihurCoder <1370054591@qq.com>
2023-02-11 23:00:54 +0800

    add module config

---
+++ spring-boot-project-mybatis-plus-dynamic-datasource/src/main/java/com/java/tutorial/project/config/MybatisPlusConfig.java
@@ -0,0 +1,37 @@
+package com.java.tutorial.project.config;
+
+import com.baomidou.mybatisplus.extension.spring.MybatisSqlSessionFactoryBean;
+import org.apache.ibatis.plugin.Interceptor;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.annotation.MapperScan;
+import org.mybatis.spring.transaction.SpringManagedTransactionFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+import javax.sql.DataSource;
+
+@Configuration
+@MapperScan("mappper所在包")
+public class MybatisPlusConfig {
+
+
+    /**
+     * 分页插件*/
+    @Bean
+    public PaginationInterceptor paginationInterceptor(){
+        return new PaginationInterceptor();
+    }
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+        MybatisSqlSessionFactoryBean sqlSessionFactoryBean = new MybatisSqlSessionFactoryBean();
+        sqlSessionFactoryBean.setDataSource(dataSource);
+        sqlSessionFactoryBean.setMapperLocations(new PathMatchingResourcePatternResolver().getResources("classpath*:mapper/*/*.xml"));
+        sqlSessionFactoryBean.setTransactionFactory(new SpringManagedTransactionFactory());
+        sqlSessionFactoryBean.setPlugins(new Interceptor[]{paginationInterceptor()});
+        return sqlSessionFactoryBean.getObject();
+    }
+
+
+}
