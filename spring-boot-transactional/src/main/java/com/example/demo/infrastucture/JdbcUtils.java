5bcf371eae648e227e6a097e596a5321ccbd2ca4
yihurCoder <1370054591@qq.com>
2023-04-13 21:16:00 +0800

    function  config

---
+++ spring-boot-transactional/src/main/java/com/example/demo/infrastucture/JdbcUtils.java
@@ -0,0 +1,81 @@
+package com.example.demo.infrastucture;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
+
+
+/**
+ * @author HY
+ */
+@Slf4j
+@Component
+public class JdbcUtils {
+
+
+    @Value("${spring.datasource.driverClassName}")
+    private String jdbcDriver;
+
+    @Value("${spring.datasource.url}")
+    private String masterDataUrlAddress;
+
+    @Value("${spring.datasource.username}")
+    private String getMasterDataUsername;
+
+    @Value("${spring.datasource.password}")
+    private String getMasterDataPassword;
+
+
+    public void batchGenerateMysqlUtil(List<Map<String, Object>> dataList, List<String> typeSortList, String insertSql) {
+        PreparedStatement stat = null;
+        Connection conn = null;
+        if (dataList == null) {
+            return;
+        }
+        try {
+            Class.forName(jdbcDriver);
+            conn = DriverManager.getConnection(masterDataUrlAddress, getMasterDataUsername, getMasterDataPassword);
+            conn.setAutoCommit(false);
+            stat = conn.prepareStatement(insertSql);
+            for (Map<String, Object> valueBean : dataList) {
+                int i = 1;
+                for (String key : typeSortList) {
+                    String val = "";
+                    if (valueBean.get(key) != null) {
+                        val = valueBean.get(key).toString();
+                    }
+                    stat.setString(i, val);
+                    i++;
+                }
+                stat.addBatch();
+            }
+            stat.executeBatch();
+            conn.commit();
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            try {
+                assert conn != null;
+                conn.rollback();
+            } catch (SQLException ex) {
+                throw new RuntimeException(ex);
+            }
+        } finally {
+            try {
+                if (stat != null) {
+                    stat.close();
+                    conn.commit();
+                }
+            } catch (SQLException e) {
+                log.error(e.getMessage());
+            }
+        }
+    }
+
+}
