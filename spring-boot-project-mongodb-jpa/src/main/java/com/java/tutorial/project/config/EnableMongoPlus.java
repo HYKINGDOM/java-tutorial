33be3e37f33b5fef644a33f8cb8a4a92a664d2f3
singleKing <1370054591@qq.com>
2023-08-16 21:12:51 +0800

    pom 调整

--- spring-boot-project-mongodb-jpa/src/main/java/com/java/tutorial/project/config/EnableMongoPlus.java
+++ spring-boot-project-mongodb-jpa/src/main/java/com/java/tutorial/project/config/EnableMongoPlus.java
@@ -8,7 +8,7 @@ import java.lang.annotation.*;
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 @Inherited
-@Import({MongoPlusAutoConfiguration.class})
+//@Import({MongoPlusAutoConfiguration.class})
 public @interface EnableMongoPlus {
 
 }
