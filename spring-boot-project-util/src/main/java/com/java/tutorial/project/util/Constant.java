89ce18896b97f08bce79e4d222bddcc5caf8fa48
huyi <huyi@wjinfo.cn>
2023-10-26 17:19:11 +0800

    ip util

---
+++ spring-boot-project-util/src/main/java/com/java/tutorial/project/util/Constant.java
@@ -0,0 +1,19 @@
+package com.java.tutorial.project.util;
+
+public interface Constant {
+
+    /**
+     * IP地址查询
+     */
+    String IP_URL = "http://whois.pconline.com.cn/ipJson.jsp";
+
+    /**
+     * 本地ip
+     */
+    String LOCAL_IP = "127.0.0.1";
+
+    /**
+     * 数据库访问地址
+     */
+    String DB_PATH = "springboot-ip/src/main/resources/ip2region/ip2region.xdb";
+}
---
+++ spring-boot-project-util/src/main/java/com/java/tutorial/project/util/IpUtil.java
@@ -0,0 +1,94 @@
+package com.java.tutorial.project.util;
+
+import static com.java.tutorial.project.util.Constant.DB_PATH;
+import static com.java.tutorial.project.util.Constant.LOCAL_IP;
+
+import jakarta.servlet.http.HttpServletRequest;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import lombok.extern.slf4j.Slf4j;
+import org.lionsoul.ip2region.xdb.Searcher;
+
+/**
+ * @author coderJim
+ * @date 2023-10-16 11:02
+ */
+@Slf4j
+public class IpUtil {
+
+    protected IpUtil() {
+    }
+
+    /**
+     * 获取 IP地址 使用 Nginx等反向代理软件， 则不能通过 request.getRemoteAddr()获取 IP地址 如果使用了多级反向代理的话，X-Forwarded-For的值并不止一个，而是一串IP地址，
+     * X-Forwarded-For中第一个非 unknown的有效IP字符串，则为真实IP地址
+     */
+    public static String getIpAddr(HttpServletRequest request) {
+        String ipAddress;
+        try {
+            ipAddress = request.getHeader("x-forwarded-for");
+            if (ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {
+                ipAddress = request.getHeader("Proxy-Client-IP");
+            }
+            if (ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {
+                ipAddress = request.getHeader("WL-Proxy-Client-IP");
+            }
+            if (ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {
+                ipAddress = request.getRemoteAddr();
+                if (ipAddress.equals(LOCAL_IP)) {
+                    // 根据网卡取本机配置的IP
+                    InetAddress inet = null;
+                    try {
+                        inet = InetAddress.getLocalHost();
+                    } catch (UnknownHostException e) {
+                        log.error(e.getMessage(), e);
+                    }
+                    ipAddress = inet.getHostAddress();
+                }
+            }
+            // 对于通过多个代理的情况，第一个IP为客户端真实IP,多个IP按照','分割
+            if (ipAddress != null && ipAddress.length() > 15) { // "***.***.***.***".length()
+                // = 15
+                if (ipAddress.indexOf(",") > 0) {
+                    ipAddress = ipAddress.substring(0, ipAddress.indexOf(","));
+                }
+            }
+        } catch (Exception e) {
+            ipAddress = "";
+        }
+
+        return ipAddress;
+    }
+
+    public static String getAddr(String ip) {
+        String project_dir = System.getProperty("user.dir") + "";
+        String dbPath = project_dir + "/" + DB_PATH;
+        // 1、从 dbPath 加载整个 xdb 到内存。
+        byte[] cBuff;
+        try {
+            cBuff = Searcher.loadContentFromFile(dbPath);
+        } catch (Exception e) {
+            log.info("failed to load content from `%s`: %s\n", dbPath, e);
+            return null;
+        }
+
+        // 2、使用上述的 cBuff 创建一个完全基于内存的查询对象。
+        Searcher searcher;
+        try {
+            searcher = Searcher.newWithBuffer(cBuff);
+        } catch (Exception e) {
+            log.info("failed to create content cached searcher: %s\n", e);
+            return null;
+        }
+        // 3、查询
+        try {
+            String region = searcher.search(ip);
+            return region;
+        } catch (Exception e) {
+            log.info("failed to search(%s): %s\n", ip, e);
+        }
+        return null;
+    }
+
+}
+
