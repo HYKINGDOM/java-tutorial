5d4605d475a5be651f040b9d4154750b03726188
huyi <huyi@wjinfo.cn>
2023-12-22 17:59:10 +0800

    序列化问题处理

--- spring-boot-project-event/src/main/java/com/java/tutorial/tool/domain/Shop.java
+++ spring-boot-project-event/src/main/java/com/java/tutorial/tool/domain/Shop.java
@@ -1,7 +1,9 @@
 package com.java.tutorial.tool.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 import java.util.List;
@@ -11,6 +13,8 @@ import java.util.List;
  */
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Shop implements Serializable {
 
     private static final long serialVersionUID = -2;
--- spring-boot-project-event/src/test/java/com/java/tutorial/project/ProductTest.java
+++ spring-boot-project-event/src/test/java/com/java/tutorial/project/ProductTest.java
@@ -6,9 +6,11 @@ import com.java.tutorial.tool.domain.Product;
 import com.java.tutorial.tool.domain.Shop;
 import org.apache.commons.lang3.SerializationUtils;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.BeanUtils;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.List;
 
 public class ProductTest {
 
@@ -40,6 +42,51 @@ public class ProductTest {
 
     @Test
     public void test_demo_03() {
+
+        Product aaaaProduct = Product.builder().stock(1111).skuName("aaaa").price(new BigDecimal("2.90")).build();
+        Product bbbbProduct = Product.builder().stock(2222).skuName("bbbb").price(new BigDecimal("3.90")).build();
+        Product ccccProduct = Product.builder().stock(3333).skuName("cccc").price(new BigDecimal("4.90")).build();
+        Product ddddProduct = Product.builder().stock(4444).skuName("dddd").price(new BigDecimal("5.90")).build();
+
+        ArrayList<Product> products = Lists.newArrayList(aaaaProduct, bbbbProduct, ccccProduct, ddddProduct);
+
+        ArrayList<Product> productsd = Lists.newArrayList(aaaaProduct, ccccProduct, ddddProduct);
+
+        Shop builda = Shop.builder().shopId(1000001L).shopName("builda").products(products).build();
+
+        Shop buildb = Shop.builder().shopId(1000002L).shopName("buildb").products(productsd).build();
+
+        Shop buildc = Shop.builder().shopId(1000003L).shopName("buildc").products(productsd).build();
+
+        List<Shop> shops = Lists.newArrayList(builda, buildb, buildc);
+
+        List<Shop> shopsId1 = getShops(shops);
+        List<Shop> shopsId2 = getShops(shops);
+
+        Order c = Order.builder().orderId(20001L).shops(shopsId1).build();
+
+        Order d = Order.builder().orderId(20002L).shops(shopsId2).build();
+
+        System.out.println(c);
+        System.out.println(d);
+    }
+
+    private static List<Shop> getShops(List<Shop> shops) {
+        List<Shop> shopsId1 = Lists.newArrayList();
+
+        for (Shop shop : shops) {
+            Shop shop1 = new Shop();
+
+            //BeanUtil.copyProperties(shop, shop1, false);
+            BeanUtils.copyProperties(shop, shop1, Shop.class);
+
+            shopsId1.add(shop1);
+        }
+        return shopsId1;
+    }
+
+    @Test
+    public void test_demo_04() {
 
         Product aaaaProduct = Product.builder().stock(1111).skuName("aaaa").price(new BigDecimal("2.90")).build();
         Product bbbbProduct = Product.builder().stock(2222).skuName("bbbb").price(new BigDecimal("3.90")).build();
