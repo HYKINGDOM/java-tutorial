497b3bbbc5f0193c5ddf86388a86681d4a9077e7
HY <huyi@wjinfo.cn>
2024-11-01 14:55:04 +0800

    change code ES

--- spring-boot-project-elasticsearch/src/test/java/com/java/tutorial/project/SpringBootProjectApplicationTests.java
+++ spring-boot-project-elasticsearch/src/test/java/com/java/tutorial/project/SpringBootProjectApplicationTests.java
@@ -1,13 +1,8 @@
 package com.java.tutorial.project;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 public class SpringBootProjectApplicationTests {
 
-    @Test
-    public void contextLoads() {
-    }
+
 
 }
